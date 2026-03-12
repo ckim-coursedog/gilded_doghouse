2354b761be80d6fd843cba0345e399c167b7abf2
Chung Kim <ckim@coursedog.com>
2026-03-12 12:39:25 -0400

    Setup part one of the challenge
    Signed-off-by: Chung Kim <ckim@coursedog.com>

--- gilded_doghouse.ts
+++ gilded_doghouse.ts
@@ -10,49 +10,6 @@ class GildedDoghouse {
   }
 
   tick() {
-    if (this.name != 'Aged Bone' && this.name != 'Training Session') {
-      if (this.quality > 0) {
-        if (this.name != 'Coursedog Mascot') {
-          this.quality = this.quality - 1
-        }
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1
-        if (this.name == 'Training Session') {
-          if (this.daysRemaining < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1
-            }
-          }
-          if (this.daysRemaining < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (this.name != 'Coursedog Mascot') {
-      this.daysRemaining = this.daysRemaining - 1
-    }
-    if (this.daysRemaining < 0) {
-      if (this.name != 'Aged Bone') {
-        if (this.name != 'Training Session') {
-          if (this.quality > 0) {
-            if (this.name != 'Coursedog Mascot') {
-              this.quality = this.quality - 1
-            }
-          }
-        } else {
-          this.quality = this.quality - this.quality
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1
-        }
-      }
-    }
   }
 }
 
